7d0a10a9a1a771f7aeb661a13d2b5992fc55ff04
Warren Galyen <wgalyen@gmail.com>
2020-09-14 15:07:08 -0700

    test: 100% coverage for snapshotter

--- src/bin/snapshotter.js
+++ src/bin/snapshotter.js
@@ -3,37 +3,31 @@ import fs from 'fs';
 import logger from '../logger';
 
 export default class SnapShotter {
-  constructor({
-    latest,
-    gridUrl,
-    width = 700,
-    height = 1024,
-    browser = 'chrome'
-  }) {
-    this.latest = latest;
+  constructor({ gridUrl, width = 700, height = 1024, browser = 'chrome' }) {
     const browserCapability = browser.includes('chrome')
       ? webdriver.Capabilities.chrome
       : webdriver.Capabilities.firefox;
-    this.driver = new webdriver.Builder()
+
+    this._driver = new webdriver.Builder()
       .usingServer(gridUrl)
       .withCapabilities(browserCapability())
       .build();
 
-    this.driver
+    this._driver
       .manage()
       .window()
       .setRect({ width, height });
   }
 
+  get driver() {
+    return this._driver;
+  }
+
   async takeSnap(scenario) {
     logger.info(`Scenario: ${scenario.label}`, `Url: ${scenario.url}`);
-    await this.driver.get(scenario.url);
-    const screenShot = await this.driver.takeScreenshot();
-    fs.writeFileSync(
-      `${this.latest}/${scenario.label}.png`,
-      screenShot,
-      'base64'
-    );
+    await this._driver.get(scenario.url);
+    const screenShot = await this._driver.takeScreenshot();
+    fs.writeFileSync(`./bellsnaps/${scenario.label}.png`, screenShot, 'base64');
     await this.driver.quit();
   }
 }
