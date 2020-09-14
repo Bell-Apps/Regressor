44a297faccd93eca4f433295fc5e2ea1ab55db74
Warren Galyen <wgalyen@gmail.com>
2020-09-14 13:47:30 -0700

    feat: browser width / height

--- src/get-screenshots.js
+++ src/get-screenshots.js
@@ -1,7 +1,9 @@
 export default (SnapShotter, config) =>
   Promise.all(
     config.scenarios.map(scenario => {
-      const snap = new SnapShotter('http://localhost:4444/wd/hub');
+      const snap = new SnapShotter({
+        gridUrl: 'http://localhost:4444/wd/hub'
+      });
 
       const promises = [];
       promises.push(snap.takeSnap(scenario.url, scenario.label));
