5a1076b3e9336a78419ec523f6007e0e69d230c4
Warren Galyen <wgalyen@gmail.com>
2020-09-16 15:45:46 -0700

    test: delete remote

---
+++ src/__mocks__/s3ListObjectsFixtures.js
@@ -0,0 +1,45 @@
+export default {
+    IsTruncated: false,
+    Contents: [
+        {
+            Key: 'chrome/latest/homepage-large.png',
+            LastModified: '2020-09-16T16:13:00.000Z',
+            ETag: '9411edc59ac302c9a3e4bedcd5228cc3',
+            Size: 1131356,
+            StorageClass: 'STANDARD'
+        },
+        {
+            Key: 'chrome/baseline/homepage-large.png',
+            LastModified: '2020-09-16T16:13:00.000Z',
+            ETag: '8d31b531d854aa4428fda148077bdb22',
+            Size: 524244,
+            StorageClass: 'STANDARD'
+        },
+        {
+            Key: 'chrome/latest/category-bike-large.png',
+            LastModified: '2020-09-16T16:13:00.000Z',
+            ETag: '8d31b531d854aa4428fda148077bdb22',
+            Size: 524244,
+            StorageClass: 'STANDARD'
+        },
+        {
+            Key: 'chrome/baseline/category-bike-large.png',
+            LastModified: '2018-06-16T14:29:41.000Z',
+            ETag: '9ac293896960555adbd33157017378e8',
+            Size: 84585,
+            StorageClass: 'STANDARD'
+        },
+        {
+            Key: 'chrome/baseline/category-motorcycle-large.png',
+            LastModified: '2020-09-16T12:43:10.000Z',
+            ETag: '75f1f97edb94d46d6142a2c893d33dd4',
+            Size: 417690,
+            StorageClass: 'STANDARD'
+        }
+    ],
+    Name: 'regresspr',
+    Prefix: '',
+    MaxKeys: 1000,
+    CommonPrefixes: [],
+    KeyCount: 169
+};
