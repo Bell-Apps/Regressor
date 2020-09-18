212ce5d80f45567ed471a8ffe84c58f5c32cda0a
Warren Galyen <wgalyen@gmail.com>
2020-09-18 10:19:39 -0700

    feat: adding to configValidator to run.js

--- src/bin/run.js
+++ src/bin/run.js
@@ -16,92 +16,124 @@ import {
   clearDirectory,
   fetchRemoteComparisonImages
 } from '../comparisonActions';
+import validateConfig from '../configValidator';
 
 setupLogger();
 
-program
-  .version('0.0.1')
-  .command('snap')
-  .option(
-    '-b, --browser [browser]',
-    'Select the browser to run your tests on. E.G. chrome, firefox, etc.'
-  )
-  .option('c, --config [config]', 'Path to your config')
-  .option('r, --remote', 'Upload new baseline to remote storage')
-  .action(async options => {
-    const configPath = path.resolve(options.config);
-    const config = require(path.resolve(options.config)); // eslint-disable-line import/no-dynamic-require
-
-    if (!config) throw new Error(`Could not resolve ${configPath}`);
-
-    validateConfig('latest', options.remote, config);
-
-    config.browser = options.browser;
-    logger.info('run', 'Getting snapshots... 📸 ');
-    await createDirectories(fs, config);
-    await createBucket(config);
-    await getScreenshots(SnapShotter, config);
-    if (options.remote) await uploadRemote('latest', config);
-  });
+function handleError(err) {
+  logger.error(
+      'run',
+      '☠️️️️️️️ ☠️ ️️️️️️☠️️️️️️️ ☠️️️️️️️ ☠️️️️️️️ ☠️️️ ERROR FOUND ☠️️️️️️️ ☠️️️️️️️ ☠️️️️️️️ ☠️️️️️️️ ☠️️️️️️️ ☠️️️'
+  );
+  console.error(err);
+  process.exitCode = 1;
+  process.exit();
+}
 
 program
-  .command('update-baseline')
-  .option(
-    '-b, --browser [browser]',
-    'Select the browser to run your tests on. E.G. chrome, firefox, etc.'
-  )
-  .option('c, --config [config]', 'Path to your config')
-  .option('r, --remote', 'Upload new baseline to remote storage')
-  .action(async options => {
-    if (!options.browser) throw 'no browser specified';
-    const config = require(path.resolve(options.config)); // eslint-disable-line import/no-dynamic-require
-    config.browser = options.browser;
+    .version('0.0.1')
+    .command('snap')
+    .option(
+        '-b, --browser [browser]',
+        'Select the browser to run your tests on. E.G. chrome, firefox, etc.'
+    )
+    .option('c, --config [config]', 'Path to your config')
+    .option('r, --remote', 'Upload new baseline to remote storage')
+    .action(async options => {
+      try {
+        const config = require(path.resolve(options.config)); // eslint-disable-line import/no-dynamic-require
 
-    createDirectories(fs, config);
-    await updateBaselineShots(fs, config).catch(error => {
-      logger.error('run', error);
+        if (!config) throw Error(`Could not resolve config at path: ${config}`);
+        if (options.browser) config.browser = options.browser;
+
+        validateConfig(config, options.remote);
+
+        logger.info('run', 'Getting snapshots... 📸 ');
+        await createDirectories(fs, config);
+        await createBucket(config);
+        await getScreenshots(SnapShotter, config);
+        if (options.remote) await uploadRemote('latest', config);
+      } catch (err) {
+        handleError(err);
+      }
     });
-    if (options.remote) await uploadRemote('baseline', config);
-  });
 
 program
-  .command('compare')
-  .option(
-    '-b, --browser [browser]',
-    'Select the browser to run your tests on. E.G. chrome, firefox, etc.'
-  )
-  .option('c, --config [config]', 'Path to your config')
-  .option('r, --remote', 'Upload new baseline to remote storage')
-  .action(async options => {
-    if (!options.browser) throw 'no browser specified';
-    const config = require(path.resolve(options.config)); // eslint-disable-line import/no-dynamic-require
-    config.browser = options.browser;
-    config.remote = options.remote;
+    .command('update-baseline')
+    .option(
+        '-b, --browser [browser]',
+        'Select the browser to run your tests on. E.G. chrome, firefox, etc.'
+    )
+    .option('c, --config [config]', 'Path to your config')
+    .option('r, --remote', 'Upload new baseline to remote storage')
+    .action(async options => {
+      try {
+        const config = require(path.resolve(options.config)); // eslint-disable-line import/no-dynamic-require
 
-    createDirectories(fs, config);
-    clearDirectory(fs, config);
-    await createBucket(config);
-    await fetchRemoteComparisonImages(config);
-    await createComparisons(fs, config);
-  });
+        if (options.browser) config.browser = options.browser;
+
+        validateConfig(config, options.remote);
+
+        createDirectories(fs, config);
+        await updateBaselineShots(fs, config).catch(error => {
+          logger.error('run', error);
+        });
+        if (options.remote) await uploadRemote('baseline', config);
+      } catch (err) {
+        handleError(err);
+      }
+    });
 
 program
-  .command('generate-report')
-  .option('c, --config [config]', 'Path to your config')
-  .option('r, --remote', 'Upload new baseline to remote storage')
-  .option(
-    '-b, --browser [browser]',
-    'Select the browser to run your tests on. E.G. chrome, firefox, etc.'
-  )
-  .action(options => {
-    const config = require(path.resolve(options.config)); // eslint-disable-line import/no-dynamic-require
-    config.browser = options.browser;
+    .command('compare')
+    .option(
+        '-b, --browser [browser]',
+        'Select the browser to run your tests on. E.G. chrome, firefox, etc.'
+    )
+    .option('c, --config [config]', 'Path to your config')
+    .option('r, --remote', 'Upload new baseline to remote storage')
+    .action(async options => {
+      try {
+        const config = require(path.resolve(options.config)); // eslint-disable-line import/no-dynamic-require
 
-    const generateReport = options.remote
-      ? generateRemoteReport
-      : generateLocalReport;
+        if (options.browser) config.browser = options.browser;
+        config.remote = options.remote;
+        validateConfig(config, options.remote);
 
-    generateReport(config);
-  });
+        createDirectories(fs, config);
+        clearDirectory(fs, config);
+        await createBucket(config);
+        await fetchRemoteComparisonImages(config);
+        await createComparisons(fs, config);
+      } catch (err) {
+        handleError(err);
+      }
+    });
+
+program
+    .command('generate-report')
+    .option('c, --config [config]', 'Path to your config')
+    .option('r, --remote', 'Upload new baseline to remote storage')
+    .option(
+        '-b, --browser [browser]',
+        'Select the browser to run your tests on. E.G. chrome, firefox, etc.'
+    )
+    .action(options => {
+      try {
+        const config = require(path.resolve(options.config)); // eslint-disable-line import/no-dynamic-require
+
+        if (options.browser) config.browser = options.browser;
+
+        validateConfig(config, options.remote);
+
+        const generateReport = options.remote
+            ? generateRemoteReport
+            : generateLocalReport;
+
+        generateReport(config);
+      } catch (err) {
+        handleError(err);
+      }
+    });
 
 program.parse(process.argv);
